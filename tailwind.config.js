34c189cd2e7191cd54f733aaeaafe16c42e0b7cd
Shadab Khan <shadab.2005@gmail.com>
2022-02-20 13:50:00 +0530

    Upgrade to tailwindv3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     fontFamily: {
       sans: ['Inter'],
@@ -15,27 +14,7 @@ module.exports = {
           secondary: '#9CA9B3',
         },
       },
-      animation: {
-        'reveal-from-bottom': 'reveal-from-bottom 0.3s ease-in',
-        'reveal-from-bottom-delayed': 'reveal-from-bottom 0.35s ease-in',
-      },
-      keyframes: {
-        'reveal-from-bottom': {
-          '0%': { transform: 'translateY(50px)', opacity: '0' },
-          '50%': { opacity: '0.25' },
-          '99%': {
-            opacity: '0.75',
-          },
-          '100%': {
-            transform: 'translateY(0)',
-            opacity: '1',
-          },
-        },
-      },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
