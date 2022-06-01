8325e0b2395a3f6f49cbe660d9f15cfdbcc7d0c7
christiecc8 <christie.cc8@gmail.com>
2022-06-01 14:53:58 -0400

    Stepper styling edit

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -62,10 +62,6 @@ const Create: React.FC<{}> = () => {
       backgroundImage: 'linear-gradient(319deg, #663dff 0%, #aa00ff 37%, #cc4499 100%)',
       boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
     }),
-    ...(ownerState.completed && {
-      backgroundImage:
-        'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-    }),
   }));
   
   function CustomStepIcon(props: StepIconProps) {
