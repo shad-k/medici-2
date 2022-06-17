a0f2d3c42c4ec7599a00b0f7529ae1594e9e8a88
christiecc8 <christie.cc8@gmail.com>
2022-06-16 23:10:25 -0400

    reservations flow WIP

--- src/components/Reservations/NFTCard.tsx
+++ src/components/Reservations/NFTCard.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect} from 'react'
 import Card from '@mui/material/Card';
 
-const NFTCard: React.FC<{collection: string, index: number}> = ({collection, index}) => {
+const NFTCard: React.FC<{collection: string, index: number, onSelect: any}> = ({collection, index, onSelect}) => {
     return (
     <div className="p-2">
         <Card>
-            <img src={process.env.PUBLIC_URL + `/assets/${collection}/${index}.png`} alt={`${index}`}/>
+            <img src={process.env.PUBLIC_URL + `/assets/${collection}/${index}.png`} alt={`${index}`} onClick={event => onSelect(index)}/>
         </Card>
     </div> 
   )
