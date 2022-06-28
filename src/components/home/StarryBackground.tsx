8274e403375c7d195f0c545751e13993288b69f2
christiecc8 <christie.cc8@gmail.com>
2022-06-28 16:19:51 -0400

    rocket moved and some design things (WIP)

--- src/components/home/StarryBackground.tsx
+++ src/components/home/StarryBackground.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Rocket from '../svgComponents/Rocket'
+import Rocket from '../../rocket.png'
 import Star1 from '../svgComponents/Star1'
 import Star2 from '../svgComponents/Star2'
 import Planet from '../svgComponents/Planet'
@@ -13,20 +13,11 @@ const StarryBackground: React.FC<{}> = () => {
 
   return (
     <div className="absolute top-0 left-0 z-0 h-full w-full flex flex-col">
-      <div className="main-page-bg flex w-full flex-1 relative"></div>
-        <div className="absolute left-[50%] top-48">
-          <Rocket/>
+      <div className="main-page-bg w-full h-full flex flex-1">
+        <div className="absolute flex bottom-0 right-0">
+          <img src={Rocket} className="transform scale-75 translate-y-18 md:scale-100"/>
         </div>
-        <div className="absolute left-[40%] top-72">
-          <Star1/>
-        </div>
-        <div className="absolute left-[90%] top-56">
-          <Planet/>
-        </div>
-        <div className="absolute left-[50%] top-48">
-          <Star2/>
-        </div>
-
+      </div>
     </div>
   )
 }
