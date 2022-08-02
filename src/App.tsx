e74f75d57450368804f9de85b138052d0478c9fe
christiecc8 <christie.cc8@gmail.com>
2022-08-02 17:45:52 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 
-import Home from './pages/home'
-import DropEditor from './pages/dropeditor'
-import Project from './pages/project'
-import ProjectManager from './pages/project_manager'
-import Creator from './pages/creator'
-import Reservation from './pages/reservation'
+import Home from './pages/home';
+import DropEditor from './pages/dropeditor';
+import Project from './pages/project';
+import ProjectManager from './pages/project_manager';
+import Creator from './pages/creator';
+import Reservation from './pages/reservation';
+import info from '../package.json';
 
-import Header from './components/header/Header'
-import WalletContextProvider from './contexts/WalletContextProvider'
-import Background from './components/Background'
-import ClaimPage from './pages/claimPage'
-import EmptyPage from './pages/emptyPage'
-import { createTheme, ThemeProvider } from '@mui/material'
-import StarryBackground from './components/home/StarryBackground'
-import party from 'party-js'
+import Header from './components/header/Header';
+import WalletContextProvider from './contexts/WalletContextProvider';
+import Background from './components/Background';
+import ClaimPage from './pages/claimPage';
+import EmptyPage from './pages/emptyPage';
+import StarryBackground from './components/home/StarryBackground';
+import party from 'party-js';
 
 const customTheme = createTheme({
   palette: {
@@ -25,19 +26,20 @@ const customTheme = createTheme({
   typography: {
     fontFamily: 'Poppins',
   },
-})
+});
 
-const sparkleConfig  = {
+const sparkleConfig = {
   lifetime: party.variation.range(0.5, 1.0),
   size: party.variation.range(0.5, 0.8),
   count: 15,
-}
+};
 
-document.body.addEventListener("click", event => {
-  party.sparkles(event, sparkleConfig)
-})
+document.body.addEventListener('click', (event) => {
+  party.sparkles(event, sparkleConfig);
+});
 
 const App: React.FC<{}> = () => {
+  console.log('Starting Launch by Medici Version : ', info.version);
   return (
     <ThemeProvider theme={customTheme}>
       <WalletContextProvider>
@@ -45,7 +47,7 @@ const App: React.FC<{}> = () => {
           <Router>
             <main className="font-sans mt-16">
               <Background />
-              <StarryBackground/>
+              <StarryBackground />
               <Header />
               <div className="z-1 relative">
                 <Routes>
@@ -53,10 +55,7 @@ const App: React.FC<{}> = () => {
                   <Route path="/launch" element={<DropEditor />} />
                   <Route path="/page/:name" element={<ClaimPage />} />
                   <Route path="/projects" element={<ProjectManager />} />
-                  <Route
-                    path="/project/:contractname"
-                    element={<Project />}
-                  />
+                  <Route path="/project/:contractname" element={<Project />} />
                   <Route path="/reservation/:name" element={<Reservation />} />
                   <Route path="/" element={<Home />} />
                   <Route path="*" element={<EmptyPage />} />
@@ -67,7 +66,7 @@ const App: React.FC<{}> = () => {
         </div>
       </WalletContextProvider>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/dropEditor/DrawerAccordions.tsx
+++ src/components/dropEditor/DrawerAccordions.tsx
@@ -28,399 +28,240 @@ const DrawerAccordions: React.FC<Props> = ({
   changeFormState,
   setAccordion,
 }) => {
-    switch(formState.tier) {
-    case TemplateTier.LOW:
-    return (
-      <Box>
-        <Accordion expanded={expandedAccordion === Accordions.DETAIL}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.DETAIL ? '' : Accordions.DETAIL
-              )
+  const details = (
+    <Accordion expanded={expandedAccordion === Accordions.DETAIL}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        onClick={() =>
+          setAccordion(
+            expandedAccordion === Accordions.DETAIL ? '' : Accordions.DETAIL
+          )
+        }
+      >
+        Details
+      </AccordionSummary>
+      <AccordionDetails>
+        <div>
+          <TextField
+            id="input-artist"
+            label="Artist"
+            variant="outlined"
+            onChange={(event) => changeFormState('artist', event.target.value)}
+            value={formState.artist}
+            sx={{
+              width: '100%',
+            }}
+          />
+        </div>
+        <div>
+          <TextField
+            id="input-description"
+            label="Description"
+            multiline
+            onChange={(event) =>
+              changeFormState('description', event.target.value)
             }
+            rows={10}
+            value={formState.description}
+            sx={{
+              width: '100%',
+              mt: 2,
+            }}
+          />
+        </div>
+      </AccordionDetails>
+    </Accordion>
+  );
+
+  const socials = (
+    <Accordion expanded={expandedAccordion === Accordions.SOCIAL}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        onClick={() =>
+          setAccordion(
+            expandedAccordion === Accordions.SOCIAL ? '' : Accordions.SOCIAL
+          )
+        }
+      >
+        Social Links
+      </AccordionSummary>
+      <AccordionDetails>
+        <Stack>
+          <TextField
+            id="input-twitter"
+            label="Twitter"
+            type="text"
+            onChange={(event) => changeFormState('twitter', event.target.value)}
+            value={formState.twitter}
+            sx={{
+              width: '100%',
+            }}
+          />
+          <TextField
+            id="input-discord"
+            label="Discord"
+            type="text"
+            onChange={(event) => changeFormState('discord', event.target.value)}
+            value={formState.discord}
+            sx={{
+              width: '100%',
+              my: 2,
+            }}
+          />
+          <TextField
+            id="input-email"
+            label="Email"
+            type="email"
+            onChange={(event) => changeFormState('email', event.target.value)}
+            value={formState.email}
+            sx={{
+              width: '100%',
+            }}
+          />
+        </Stack>
+      </AccordionDetails>
+    </Accordion>
+  );
+
+  const fonts = (
+    <Accordion expanded={expandedAccordion === Accordions.FONT}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        onClick={() =>
+          setAccordion(
+            expandedAccordion === Accordions.FONT ? '' : Accordions.FONT
+          )
+        }
+      >
+        Font
+      </AccordionSummary>
+      <AccordionDetails>
+        <FontPicker
+          apiKey={process.env.REACT_APP_GOOGLE_FONTS_API_KEY!}
+          activeFontFamily={formState.fontFamily}
+          onChange={(nextFont: any) =>
+            changeFormState('fontFamily', nextFont.family)
+          }
+        />
+      </AccordionDetails>
+    </Accordion>
+  );
+
+  const tiers = (
+    <Accordion expanded={expandedAccordion === Accordions.TIER}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        onClick={() =>
+          setAccordion(
+            expandedAccordion === Accordions.TIER ? '' : Accordions.TIER
+          )
+        }
+      >
+        Claim Tier
+      </AccordionSummary>
+      <AccordionDetails>
+        <FormControl>
+          <RadioGroup
+            value={formState.tier}
+            name="claim-tier"
+            id="tier-select"
+            onChange={(event) => changeFormState('tier', event.target.value)}
           >
-            Details
-          </AccordionSummary>
-          <AccordionDetails>
-            <div>
-              <TextField
-                id="input-artist"
-                label="Artist"
-                variant="outlined"
-                onChange={(event) =>
-                  changeFormState('artist', event.target.value)
-                }
-                value={formState.artist}
-                sx={{
-                  width: '100%',
-                }}
-              />
-            </div>
-            <div>
-              <TextField
-                id="input-description"
-                label="Description"
-                multiline
-                onChange={(event) =>
-                  changeFormState('description', event.target.value)
-                }
-                rows={10}
-                value={formState.description}
-                sx={{
-                  width: '100%',
-                  mt: 2,
-                }}
-              />
-            </div>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.SOCIAL}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.SOCIAL ? '' : Accordions.SOCIAL
-              )
-            }
-          >
-            Social Links
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack>
-              <TextField
-                id="input-twitter"
-                label="Twitter"
-                type="text"
-                onChange={(event) =>
-                  changeFormState('twitter', event.target.value)
-                }
-                value={formState.twitter}
-                sx={{
-                  width: '100%',
-                }}
-              />
-              <TextField
-                id="input-discord"
-                label="Discord"
-                type="text"
-                onChange={(event) =>
-                  changeFormState('discord', event.target.value)
-                }
-                value={formState.discord}
-                sx={{
-                  width: '100%',
-                  my: 2,
-                }}
-              />
-              <TextField
-                id="input-email"
-                label="Email"
-                type="email"
-                onChange={(event) => changeFormState('email', event.target.value)}
-                value={formState.email}
-                sx={{
-                  width: '100%',
-                }}
-              />
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.FONT}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.FONT ? '' : Accordions.FONT
-              )
-            }
-          >
-            Font
-          </AccordionSummary>
-          <AccordionDetails>
-            <FontPicker
-              apiKey={process.env.REACT_APP_GOOGLE_FONTS_API_KEY!}
-              activeFontFamily={formState.fontFamily}
-              onChange={(nextFont: any) =>
-                changeFormState('fontFamily', nextFont.family)
+            <FormControlLabel value="free" control={<Radio />} label="Free" />
+            <FormControlLabel value="low" control={<Radio />} label="Low" />
+            <FormControlLabel value="music" control={<Radio />} label="Music" />
+            <FormControlLabel
+              value="mid"
+              control={<Radio />}
+              label="Mid (coming soon!)"
+              disabled
+            />
+            <FormControlLabel
+              value="high"
+              control={<Radio />}
+              label="High (coming soon!)"
+              disabled
+            />
+          </RadioGroup>
+        </FormControl>
+      </AccordionDetails>
+    </Accordion>
+  );
+
+  const colors = (
+    <Accordion expanded={expandedAccordion === Accordions.COLORS}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        onClick={() =>
+          setAccordion(
+            expandedAccordion === Accordions.COLORS ? '' : Accordions.COLORS
+          )
+        }
+      >
+        Colors
+      </AccordionSummary>
+      <AccordionDetails>
+        <Stack spacing={2}>
+          <div id="primary-color-picker">
+            <label htmlFor="primary-color-picker" className="block">
+              Primary Color
+            </label>
+            <ColorPicker
+              value={formState.primaryColor}
+              onChange={(event) =>
+                changeFormState('primaryColor', `#${event.hex}`)
               }
             />
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.TIER}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.TIER ? '' : Accordions.TIER
-              )
-            }
-          >
-            Claim Tier
-          </AccordionSummary>
-          <AccordionDetails>
-            <FormControl>
-              <RadioGroup
-                value={formState.tier}
-                name="claim-tier"
-                id="tier-select"
-                onChange={(event) => changeFormState('tier', event.target.value)}
-              >
-                <FormControlLabel value="free" control={<Radio />} label="Free" />
-                <FormControlLabel value="low" control={<Radio />} label="Low" />
-                <FormControlLabel
-                  value="mid"
-                  control={<Radio />}
-                  label="Mid (coming soon!)"
-                  disabled
-                />
-                <FormControlLabel
-                  value="high"
-                  control={<Radio />}
-                  label="High (coming soon!)"
-                  disabled
-                />
-              </RadioGroup>
-            </FormControl>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.COLORS}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.COLORS ? '' : Accordions.COLORS
-              )
-            }
-          >
-            Colors
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack spacing={2}>
-              <div id="primary-color-picker">
-                <label htmlFor="primary-color-picker" className="block">
-                  Primary Color
-                </label>
-                <ColorPicker
-                  value={formState.primaryColor}
-                  onChange={(event) =>
-                    changeFormState('primaryColor', `#${event.hex}`)
-                  }
-                />
-              </div>
-              <div id="secondary-color-picker">
-                <label htmlFor="secondary-color-picker" className="block">
-                  Secondary Color
-                </label>
-                <ColorPicker
-                  value={formState.secondaryColor}
-                  onChange={(event) =>
-                    changeFormState('secondaryColor', `#${event.hex}`)
-                  }
-                />
-              </div>
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-      </Box>
-    );
+          </div>
+          <div id="secondary-color-picker">
+            <label htmlFor="secondary-color-picker" className="block">
+              Secondary Color
+            </label>
+            <ColorPicker
+              value={formState.secondaryColor}
+              onChange={(event) =>
+                changeFormState('secondaryColor', `#${event.hex}`)
+              }
+            />
+          </div>
+        </Stack>
+      </AccordionDetails>
+    </Accordion>
+  );
+
+  const audio = null;
+
+  switch (formState.tier) {
+    case TemplateTier.LOW:
+      return (
+        <Box>
+          {tiers}
+          {details}
+          {socials}
+          {fonts}
+          {colors}
+        </Box>
+      );
+    case TemplateTier.MUSIC:
+      return (
+        <Box>
+          {tiers}
+          {details}
+          {socials}
+          {fonts}
+          {colors}
+          {audio}
+        </Box>
+      );
     default:
       return (
-      <Box>
-        {/* <Accordion expanded={expandedAccordion === Accordions.DETAIL}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.DETAIL ? '' : Accordions.DETAIL
-              )
-            }
-          >
-            Details
-          </AccordionSummary>
-          <AccordionDetails>
-            <div>
-              <TextField
-                id="input-artist"
-                label="Artist"
-                variant="outlined"
-                onChange={(event) =>
-                  changeFormState('artist', event.target.value)
-                }
-                value={formState.artist}
-                sx={{
-                  width: '100%',
-                }}
-              />
-            </div>
-            <div>
-              <TextField
-                id="input-description"
-                label="Description"
-                multiline
-                onChange={(event) =>
-                  changeFormState('description', event.target.value)
-                }
-                rows={10}
-                value={formState.description}
-                sx={{
-                  width: '100%',
-                  mt: 2,
-                }}
-              />
-            </div>
-          </AccordionDetails>
-        </Accordion> */}
-        <Accordion expanded={expandedAccordion === Accordions.SOCIAL}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.SOCIAL ? '' : Accordions.SOCIAL
-              )
-            }
-          >
-            Social Links
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack>
-              <TextField
-                id="input-twitter"
-                label="Twitter"
-                type="text"
-                onChange={(event) =>
-                  changeFormState('twitter', event.target.value)
-                }
-                value={formState.twitter}
-                sx={{
-                  width: '100%',
-                }}
-              />
-              <TextField
-                id="input-discord"
-                label="Discord"
-                type="text"
-                onChange={(event) =>
-                  changeFormState('discord', event.target.value)
-                }
-                value={formState.discord}
-                sx={{
-                  width: '100%',
-                  my: 2,
-                }}
-              />
-              <TextField
-                id="input-email"
-                label="Email"
-                type="email"
-                onChange={(event) => changeFormState('email', event.target.value)}
-                value={formState.email}
-                sx={{
-                  width: '100%',
-                }}
-              />
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.FONT}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.FONT ? '' : Accordions.FONT
-              )
-            }
-          >
-            Font
-          </AccordionSummary>
-          <AccordionDetails>
-            <FontPicker
-              apiKey={process.env.REACT_APP_GOOGLE_FONTS_API_KEY!}
-              activeFontFamily={formState.fontFamily}
-              onChange={(nextFont: any) =>
-                changeFormState('fontFamily', nextFont.family)
-              }
-            />
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.TIER}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.TIER ? '' : Accordions.TIER
-              )
-            }
-          >
-            Claim Tier
-          </AccordionSummary>
-          <AccordionDetails>
-            <FormControl>
-              <RadioGroup
-                value={formState.tier}
-                name="claim-tier"
-                id="tier-select"
-                onChange={(event) => changeFormState('tier', event.target.value)}
-              >
-                <FormControlLabel value="free" control={<Radio />} label="Free" />
-                <FormControlLabel value="low" control={<Radio />} label="Low" />
-                <FormControlLabel
-                  value="mid"
-                  control={<Radio />}
-                  label="Mid (coming soon!)"
-                  disabled
-                />
-                <FormControlLabel
-                  value="high"
-                  control={<Radio />}
-                  label="High (coming soon!)"
-                  disabled
-                />
-              </RadioGroup>
-            </FormControl>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expandedAccordion === Accordions.COLORS}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            onClick={() =>
-              setAccordion(
-                expandedAccordion === Accordions.COLORS ? '' : Accordions.COLORS
-              )
-            }
-          >
-            Colors
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack spacing={2}>
-              <div id="primary-color-picker">
-                <label htmlFor="primary-color-picker" className="block">
-                  Primary Color
-                </label>
-                <ColorPicker
-                  value={formState.primaryColor}
-                  onChange={(event) =>
-                    changeFormState('primaryColor', `#${event.hex}`)
-                  }
-                />
-              </div>
-              {/* <div id="secondary-color-picker">
-                <label htmlFor="secondary-color-picker" className="block">
-                  Secondary Color
-                </label>
-                <ColorPicker
-                  value={formState.secondaryColor}
-                  onChange={(event) =>
-                    changeFormState('secondaryColor', `#${event.hex}`)
-                  }
-                />
-              </div> */}
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-      </Box>
-    );
+        <Box>
+          {tiers}
+          {socials}
+          {fonts}
+          {colors}
+        </Box>
+      );
   }
 };
 
